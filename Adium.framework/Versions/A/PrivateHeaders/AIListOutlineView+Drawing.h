abc033b2052f3e54daaf254450feca07c4ec2455
Rahul AG <agrahul@gmail.com>
2009-10-20 22:13:11 +0800

    Whoops. Including frameworks was unintended.

--- Adium.framework/Versions/A/PrivateHeaders/AIListOutlineView+Drawing.h
+++
@@ -1,9 +0,0 @@
-//
-//  AIListOutlineView+Drawing.h
-//  Adium
-//
-//  Created by Evan Schoenberg on 4/9/09.
-//  Copyright 2009 Adium X / Saltatory Software. All rights reserved.
-//
-
-#import <Adium/AIListOutlineView.h>
